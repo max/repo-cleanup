0115a4ecc2bc8bbdc285019328aca4182e705fcf
Max Schoening <max@max.wtf>
2019-04-05 18:58:11 -0700

    Implement basic repo archiving

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const { Toolkit } = require("actions-toolkit");
 
-Toolkit.run(async tools => {
-  const repos = await tools.github.repos.list();
-  console.log(repos);
+Toolkit.run(tools => {
+  const options = tools.github.repos.list.endpoint.merge({
+    affiliation: "owner"
+  });
+
+  tools.github
+    .paginate(options, response => response.data)
+    .then(repos => repos.filter(repo => repo.name.startsWith("tmp-")))
+    .then(repos => repos.filter(repo => !repo.archived))
+    .then(repos =>
+      repos.map(repo =>
+        tools.github.repos.update({
+          owner: repo.owner.login,
+          repo: repo.name,
+          name: repo.name,
+          archived: true
+        })
+      )
+    );
 });
